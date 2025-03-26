1bc49340e9610ae33c9c5d887b1b7942a5a2c531
Varun Kumar <varunkumar@Varuns-MacBook-Air.local>
2025-03-26 00:51:04 -0400

    add react-router-dom5.3.4 and react-router-bootstrap0.25.0

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { LinkContainer, LinkContainers } from "react-router-bootstrap";
 
 import React from "react";
 
@@ -7,12 +8,22 @@ export default function Header() {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container fluid>
-          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="navbarScroll">
             <Nav className="mr-auto">
-              <Nav.Link href="cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-              <Nav.Link href="login"><i className="fas fa-user"></i>Login</Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link href="cart">
+                  <i className="fas fa-shopping-cart"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-user"></i>Login
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
